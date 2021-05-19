af1c74b3f6847f72b1dfb8821bf51a9866008cac
nik <grim322@bk.ru>
2021-05-19 11:12:30 +0300

    tasks for hw_2 added

---
+++ hw_2/task_5.js
@@ -0,0 +1,29 @@
+// Реализовать четыре основные арифметические операции в виде функций с двумя параметрами. 
+// Обязательно использовать оператор return.
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+
+let a = 10;
+let b = 5;
+
+
+console.log(add(a, b));
+console.log(sub(a, b));
+console.log(mult(a, b));
+console.log(div(a, b));
